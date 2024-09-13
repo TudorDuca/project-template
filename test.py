fc0617aedc8a844abf9918b385ed843bd4677a85
TudorDuca <petudu@alumni.uv.es>
2024-09-13 21:48:47 +0200

    Add second DataFrame for additional data analysis

--- test.py
+++ test.py
@@ -3,3 +3,5 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 df = pd.DataFrame(np.random.randn(100, 2), columns=["A", "B"])
+
+df2 = pd.DataFrame(np.random.randn(100, 2), columns=["A", "B"])
